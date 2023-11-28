9d99056ac36ec571c25146770f7e52d30780c3f8
Ashok Kumar Munaganti <Ashok>
2023-11-28 20:33:43 +0530

    project added

---
+++ src/main/java/in/ashokit/controller/WelcomeController.java
@@ -0,0 +1,24 @@
+package in.ashokit.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	private Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+	public WelcomeController() {
+		logger.info("***** WelcomeRestController::Constructor *****");
+	}
+
+	@GetMapping
+	public String welcomeMsg() {
+		logger.info("***** welcomeMsg() execution start *****");
+		String msg = "Welcome to Ashok IT - Software Training Institute..!!";
+		logger.info("***** welcomeMsg() execution end *****");
+		return msg;
+	}
+}
